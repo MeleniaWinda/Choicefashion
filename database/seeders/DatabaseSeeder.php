a82029602ec892d476ae6b6f8b7cef5af8623c7e
MELE <meleniawinda@gmail.com>
2021-07-23 16:07:50 +0700

    database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+		DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'Admin',
+            'email' => 'admin@app.com',
+            'password' => '$2y$10$q1XRGEsKBoYEoKTt21Sp4uSKBavhuy6u/dE2r7N5kDjBRu5mac1de',
+            'created_at' => '2021-07-23 12:00:00',
+            'updated_at' => '2021-07-23 12:00:00'
+        ]);
     }
 }
